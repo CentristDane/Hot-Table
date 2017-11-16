02c82dcd98b17c0c7636e1c246271d1bf90b65de
Andrew Lipsey <adlipsey@gmail.com>
2017-11-16 13:05:59 -0500

    Reserve.html done

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/add", function(req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
-app.get("/add", function(req, res) {
+app.get("/tables", function(req, res) {
   res.sendFile(path.join(__dirname, "tables.html"));
 });
 
