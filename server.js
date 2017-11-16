8507a833d99566fadb0e8cfead2dbbf810fbe7ec
Dane Sherrets <Dane@Danes-MacBook-Air.local>
2017-11-16 12:48:42 -0500

    java 1

---
+++ server.js
@@ -0,0 +1,51 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var reservationInfo = [];
+var waitingList = [];
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "client.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+
+////post function
+app.post("/api/new", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newReservation = req.body;
+  newReservation = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newReservation);
+
+
+  res.json(newReservation);
+  if (reservationInfo.length <= 4) {
+  	reservationInfo.push(newReservation);
+  } else {
+
+  	waitingList.push(newReservation);
+
+  }
+});
